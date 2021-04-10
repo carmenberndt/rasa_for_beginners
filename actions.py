3c65d6d48acad6a719da9e85e6b03cd6156110fc
Carmen Berndt <carmen@berndt-home.de>
2021-04-10 21:25:41 +0200

    fixes

--- actions.py
+++ actions.py
@@ -12,12 +12,13 @@ import os
 
 load_dotenv()
 
-airtable_api_key=os.getenv("AIRTABLE_API_KEY")
-base_id=os.getenv("BASE_ID")
-table_name=os.getenv("TABLE_NAME")
+airtable_api_key = os.getenv("AIRTABLE_API_KEY")
+base_id = os.getenv("BASE_ID")
+table_name = os.getenv("TABLE_NAME")
+
 
 def create_health_log(confirm_exercise, exercise, sleep, diet, stress, goal):
-    request_url=f"https://api.airtable.com/v0/{base_id}/{table_name}"
+    request_url = f"https://api.airtable.com/v0/{base_id}/{table_name}"
 
     headers = {
         "Content-Type": "application/json",
@@ -44,16 +45,17 @@ def create_health_log(confirm_exercise, exercise, sleep, diet, stress, goal):
     except requests.exceptions.HTTPError as err:
         raise SystemExit(err)
 
+
 class ValidateHealthForm(FormValidationAction):
     def name(self) -> Text:
         return "validate_health_form"
 
     async def validate_confirm_exercise(
-        self,
-        value: Text,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any],
+            self,
+            value: Text,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any],
     ) -> Dict[Text, Any]:
         if value:
             return {"confirm_exercise": True}
@@ -66,10 +68,10 @@ class ActionSubmitResults(Action):
         return "action_submit_results"
 
     def run(
-        self,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any],
+            self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any],
     ) -> List[Dict]:
         confirm_exercise = tracker.get_slot("confirm_exercise")
         exercise = tracker.get_slot("exercise")
